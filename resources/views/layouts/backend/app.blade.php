9204128042118ce5a6075855d01ae70355fa7aa9
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-10-21 12:47:48 +0600

    Name

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -12,7 +12,7 @@
     <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
-            <p class="loader__label">Elite admin</p>
+            <p class="loader__label">Md. Sakir Ahmed</p>
         </div>
     </div>
     <!-- ============================================================== -->
